65e0fc71f2d7be4c94932bde1f70c77ed5e2c464
ci-robot <ci-robot@k8s.io>
2023-03-07 21:42:34 +0000

    Update to ACK runtime `v0.24.1`, code-generator `v0.24.3`

--- pkg/resource/cache_policy/sdk.go
+++ pkg/resource/cache_policy/sdk.go
@@ -77,6 +77,9 @@ func (rm *resourceManager) sdkFind(
 	resp, err = rm.sdkapi.GetCachePolicyWithContext(ctx, input)
 	rm.metrics.RecordAPICall("READ_ONE", "GetCachePolicy", err)
 	if err != nil {
+		if reqErr, ok := ackerr.AWSRequestFailure(err); ok && reqErr.StatusCode() == 404 {
+			return nil, ackerr.NotFound
+		}
 		if awsErr, ok := ackerr.AWSError(err); ok && awsErr.Code() == "NoSuchCachePolicy" {
 			return nil, ackerr.NotFound
 		}
