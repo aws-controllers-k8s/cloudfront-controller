4283f405423380b66b2f8e891df928a83eea6b1a
knottnt <trevor.knottnerus@gmail.com>
2025-05-01 16:33:35 -0700

    Revert removal of ETag update hook

--- pkg/resource/vpc_origin/sdk.go
+++ pkg/resource/vpc_origin/sdk.go
@@ -476,6 +476,14 @@ func (rm *resourceManager) sdkUpdate(
 	}
 
 	rm.setStatusDefaults(ko)
+	// We need to do this manually because the ETag field (required for the
+	// IfMatch field when updating or deleting a resource in CloudFront) is
+	// outside the wrapper field of `ResponseHeadersPolicy` in the response and
+	// therefore not picked up by SetResource code generation.
+	if resp.ETag != nil {
+		ko.Status.ETag = resp.ETag
+	}
+
 	return &resource{ko}, nil
 }
 
